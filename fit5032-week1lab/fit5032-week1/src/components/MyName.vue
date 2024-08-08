3035bf2ebad95332e85507978569dc24cc612923
tniu0003 <your.email@example.com>
2024-08-08 13:22:31 +1000

    week1 lab renew code

---
+++ fit5032-week1lab/fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="my-name">
+    <h1>My name is Tianyu Niu!</h1>
+  </div>
+</template>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  color: #42b983;
+  font-family: 'Roboto', sans-serif;
+}
+</style>
