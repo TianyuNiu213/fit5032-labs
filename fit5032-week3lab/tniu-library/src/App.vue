ba25df7c5a4294921447b29b834d3e95a9077f1c
tniu0003 <your.email@example.com>
2024-08-11 23:28:12 +1000

    week3 lab code

---
+++ fit5032-week3lab/tniu-library/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <!-- <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div> -->
+    </header>
+
+    <JSON />
+
+    <main>
+      <!-- <TheWelcome /> -->
+    </main>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+}
+</style>
