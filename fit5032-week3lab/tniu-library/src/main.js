8e5097fab4025c84d0f5718ef8bf1e657213f4a6
tniu0003 <your.email@example.com>
2024-08-15 12:08:34 +1000

    week3 renew main.js

--- fit5032-week3lab/tniu-library/src/main.js
+++ fit5032-week3lab/tniu-library/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+//import './assets/main.css'
 //import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
